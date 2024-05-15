9505f3817b4e1510f9ea5e98ac61563ab859cee8
Daniel Ibáñez Martínez <126546485+danielibanezm@users.noreply.github.com>
2024-05-15 16:47:53 +0200

    comentarios hechos, faltan los likes

---
+++ StarPath/lib/model/PostData.dart
@@ -0,0 +1,14 @@
+class PostData{
+  String id_user = '-1';
+  String id_post= '-1';
+  String content = 'vacio';
+  String description = 'vacio';
+  int like = -1;
+  int dislike = -1;
+  String created_at = 'vacio';
+
+  @override
+  String toString() {
+    return 'PostData{id_user: $id_user, id_post: $id_post, content: $content, description: $description, like: $like, dislike: $dislike, created_at: $created_at}';
+  }
+}
