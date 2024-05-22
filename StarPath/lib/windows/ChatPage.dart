899612cfa8bc134c73c007d47948128efc213d82
Alvaro <alvaro.paniego.fernandez@gmail.com>
2024-05-22 15:28:22 +0200

    mensajes terminado, no es en tiempo real

--- StarPath/lib/windows/ChatPage.dart
+++ StarPath/lib/windows/ChatPage.dart
@@ -8,6 +8,7 @@ import 'package:starpath/model/user.dart';
 import 'package:starpath/model/user_data.dart';
 import 'package:starpath/widgets/message_bubble.dart';
 import 'package:starpath/widgets/upper_app_bar.dart';
+import 'package:starpath/windows/main_page.dart';
 import 'package:supabase/supabase.dart';
 
 class ChatPage extends StatefulWidget {
@@ -21,25 +22,37 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> {
   final TextEditingController _messageController = TextEditingController();
   UserData senderUser = UserData.empty();
-  var channel = supabase.channel("chat_room");
-  // Future<List<Message>> futureMessages = getFutureMessagesAsync(senderUser.id_user, widget.receiverUser.id_user);
+  late Future<List<Message>> futureMessages;
 
   @override
   void initState() {
     super.initState();
-    channel.onPostgresChanges(
-      event: PostgresChangeEvent.insert,
+    // futureMessages = getFutureMessagesAsync(senderUser.id_user, widget.receiverUser.id_user);
+    supabase.channel("chat_room").onPostgresChanges(
+      event: PostgresChangeEvent.all,
       schema: 'public',
       table: 'message',
       callback: (payload) {
-        if(payload.newRecord['id_user_receiver'] == widget.receiverUser.id_user &&
-            payload.newRecord['id_user_sender'] == senderUser.id_user){
-
+        print(payload);
+        var idReceiver = payload.newRecord['id_user_receiver'];
+        var idSender = payload.newRecord['id_user_sender'];
+        print("idSender: $idSender idReceiver: $idReceiver");
+        if(idReceiver == widget.receiverUser.id_user &&
+            idSender == senderUser.id_user){
+          var senderUserAux = getUserDataAsync(idSender);
+          String username = "";
+          senderUserAux.then((value) => username = value.username);
+          setState(() {
+            futureMessages.then((value) {
+              value.add(Message(username, payload.newRecord['message'], idSender));
+            });
+          });
         }
-      },);
+      },).subscribe();
   }
   @override
   void dispose() {
+    supabase.channel("chat_room").unsubscribe();
     super.dispose();
   }
 
@@ -47,6 +60,7 @@ class _ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     User user = context.watch<UserProvider>().user!;
     getUserDataAsync(user.id).then((value) => senderUser = value);
+    futureMessages = getFutureMessagesAsync(user.id, widget.receiverUser.id_user);
     bool hasValidImage = widget.receiverUser.profile_picture.isNotEmpty;
     return Scaffold(
         resizeToAvoidBottomInset: false,
@@ -66,15 +80,32 @@ class _ChatPageState extends State<ChatPage> {
                 ]),
             Expanded(
                 flex: 8,
-                child: ListView(
-                  children: [
-                    MessageBubble(messageData: Message("pepe", "hola"), isSender: true),
-                    MessageBubble(messageData: Message("pepe", "que tal?"), isSender: true),
-                    MessageBubble(messageData: Message("ana", "bien"), isSender: false),
-                    MessageBubble(messageData: Message("pepe", "a"), isSender: true),
-                    MessageBubble(messageData: Message("ana", "..."), isSender: false),
-                  ],
-                )),
+                child: FutureBuilder(
+                  future: futureMessages,
+                  builder: (context, snapshot) {
+                    if(snapshot.hasData){
+                      if(snapshot.data!.isEmpty){
+                        return const Center(
+                          child: Text("Escribe para empezar la conversacion :)", style: TextStyle(color: TEXT),),
+                        );
+                      }
+                      return ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          return MessageBubble(messageData: snapshot.data![index],
+                              isSender: snapshot.data![index].sender_id != user.id);
+                        });
+                    }else if(snapshot.hasError){
+                      return const Center(
+                        child: Text("Error de conexion", style: TextStyle(color: TEXT),),
+                      );
+                    }
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+                ),
+            ),
             Expanded(
                 flex: 1,
                 child: Container(
@@ -102,7 +133,7 @@ class _ChatPageState extends State<ChatPage> {
                         flex: 1,
                         child: IconButton(
                             onPressed: () async{
-                              await sendMessageAsync(senderUser.id_user,
+                              await sendMessageAsync(user.id,
                                   widget.receiverUser.id_user,
                                   _messageController.text.trim());
                               _messageController.text = "";
@@ -142,7 +173,26 @@ class _ChatPageState extends State<ChatPage> {
   }
 }
 
-Future<List<Message>> getFutureMessagesAsync(String id_user, String id_user2) async{
+Future<List<Message>> getFutureMessagesAsync(String id_userSender, String id_userReceiver) async{
   List<Message> messageList = [];
+  Message messageData;
+  print("cogiendo mensajes");
+  var res = [];
+  // print('sender $id_userSender receiver $id_userReceiver');
+  try{
+    res = await supabase.from('message').select()
+        .or('and(id_user_sender.eq.$id_userSender,id_user_receiver.eq.$id_userReceiver),and'
+        '(id_user_sender.eq.$id_userReceiver,id_user_receiver.eq.$id_userSender)');
+  }catch (e){
+    print(e);
+  }
+  for (var message in res) {
+    messageData = Message.empty();
+    messageData.message = message['message'];
+    var user = await getUserDataAsync(message['id_user_sender']);
+    messageData.user = user.username;
+    messageData.sender_id = message['id_user_sender'];
+    messageList.add(messageData);
+  }
   return messageList;
 }
